5735e2d678d2eae239c218581976c166bd874b4c
ChrissyAFK <cshen0800@icloud.com>
2024-04-18 12:15:46 -0700

    changing font

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles.css">
     <title>TETRISCX</title>
 </head>
