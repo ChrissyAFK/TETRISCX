020dcae34059f1ca4bf44a743b3a1351daf9685e
ChrissyAFK <cshen0800@icloud.com>
2024-04-18 12:41:46 -0700

    trying again  to fix logout

--- logout.php
+++ logout.php
@@ -1,20 +1,7 @@
 <?php
 session_start();
-
-// Check if the user is logged in
-if (isset($_SESSION['user_id'])) {
-    // Unset all session variables
-    $_SESSION = array();
-
-    // Destroy the session
-    session_destroy();
-
-    // Redirect to the login page or any other page you want
-    header("Location: index.php");
-    exit;
-} else {   
-    // Redirect to the login page or any other page you want
-    header("Location: index.php");
-    exit;
-}
+$_SESSION = array();
+session_destroy();
+header("Location: index.php");
+exit;
 ?>
