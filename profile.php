b2297469354bd99b4046f91303a0d490579a53e0
ChrissyAFK <cshen0800@icloud.com>
2024-04-18 16:49:12 -0700

    attempt at updating profile code

--- profile.php
+++ profile.php
@@ -1,10 +1,15 @@
 <?php
 
 // Connect to the database
-$servername = "server0800";
-$username = "your_username";
-$password = "your_password";
-$dbname = "users";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
