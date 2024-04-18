c908b759a4f2a32a212b1aa3513ef06362f3c8c6
ChrissyAFK <cshen0800@icloud.com>
2024-04-18 15:43:26 -0700

    attempt #1 of fixing game

--- game.php
+++ game.php
@@ -94,11 +94,17 @@ function getNextTetromino() {
   const name = tetrominoSequence.pop();
   const matrix = tetrominos[name];
 
-  canHold = true;
-updateNextPieceDisplay();
+  // I and O start centered, all others start in left-middle
+  const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+
+  // I starts on row 21 (-1), all others start on row 22 (-2)
+  const row = name === 'I' ? -1 : -2;
+
   return {
-    name: name,
-    matrix: matrix,
+    name: name,      // name of the piece (L, O, etc.)
+    matrix: matrix,  // the current rotation matrix
+    row: row,        // current row (starts offscreen)
+    col: col         // current col
   };
 }
 
