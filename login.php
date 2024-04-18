8bf29c8308fd789d781d72ccd6eaec14786e7dee
ChrissyAFK <cshen0800@icloud.com>
2024-04-18 12:11:10 -0700

    username not usernae

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Verify the provided password with the hashed password
         if (password_verify($password1, $hashedPassword)) {
             // Password is correct, redirect to the home page
-            $_SESSION['usernae'] = $username1;
+            $_SESSION['username'] = $username1;
             header('Location: index.php');
             exit;
         } else {
