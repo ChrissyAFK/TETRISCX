0962b134d44dbf6315fbc25e85874d0578671ad4
ChrissyAFK <cshen0800@icloud.com>
2024-05-26 21:03:07 -0700

    whoopsv14

--- leaderboard.php
+++ leaderboard.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$servername = $_ENV['DB_SERVERNAME'];
+$servername = $_ENV['DB_SERVER'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
 $dbname = $_ENV['DB_NAME'];
